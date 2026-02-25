b7dbdd401442622a906007de86dd3b1848b39666
RPG-Alex <mralexblood@outlook.com>
2026-02-25 14:08:19 +0800

    added `#[must_use]` for `SmilesParser` methods

--- src/parser/smiles_parser.rs
+++ src/parser/smiles_parser.rs
@@ -1,6 +1,6 @@
 //! Second pass that parses the [`TokenWithSpan`]
 
-use crate::{errors::SmilesError, token::TokenWithSpan};
+use crate::token::TokenWithSpan;
 
 /// Contains the vec of tokens being iterated on and tracks the current position in that vec
 pub struct SmilesParser {
@@ -10,14 +10,17 @@ pub struct SmilesParser {
 
 impl SmilesParser {
     /// Creates a new `SmilesParser` structure
+    #[must_use]
     pub fn new(tokens: Vec<TokenWithSpan>) -> Self {
         SmilesParser { tokens, position: 0 }
     }
     /// Retrieves the `tokens` field of [`Vec<TokenWithSpan>`]
+    #[must_use]
     pub fn tokens(&self) -> &[TokenWithSpan] {
         &self.tokens
     }
     /// Retrieves the current position
+    #[must_use]
     pub fn position(&self) -> usize {
         self.position
     }   
