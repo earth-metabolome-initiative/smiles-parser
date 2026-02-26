ea48fd3238349fb36d08f2334ee3fa62372ed0e9
RPG-Alex <mralexblood@outlook.com>
2026-02-26 14:31:42 +0800

    Began implementing `Smiles` struct

--- src/smiles.rs
+++ src/smiles.rs
@@ -1,6 +1,32 @@
 //! Represents a SMILES structure.
 
+use crate::{atom_node::AtomNode, bond::Bond, bond_edge::BondEdge};
+
 mod from_str;
 
 /// Represents a SMILES structure.
-pub struct Smiles {}
+pub struct Smiles {
+    atom_nodes: Vec<AtomNode>,
+    bond_edges: Vec<BondEdge>,
+}
+
+impl Smiles {
+    /// creates a new instance
+    #[must_use]
+    pub fn new() -> Self {
+        Self { atom_nodes: Vec::new(), bond_edges: Vec::new() }
+    }
+    /// Pushes an AtomNode
+    pub fn push_node(&mut self, node: AtomNode) {
+        self.atom_nodes.push(node);
+    }
+    /// adds an edge from two nodes and the [`Bond`]
+    pub fn push_edge(&mut self, node_a: usize, node_b: usize, bond: Bond) {
+        let bond_edge = BondEdge::new(node_a, node_b, bond);
+        self.bond_edges.push(bond_edge);
+    }
+}
+
+impl Default for Smiles {
+    fn default() -> Self { Self::new() }
+}
