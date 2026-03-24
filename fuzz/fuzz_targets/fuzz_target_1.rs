df3f3c9497a5535834bd5c9fad3fd4b17e08d5b1
RPG-Alex <mralexblood@outlook.com>
2026-03-24 15:40:22 +0800

    updated fuzzzer

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,14 +1,18 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use std::str;
 use smiles_parser::smiles::Smiles;
 
 fuzz_target!(|data: &str| {
     if let Ok(smiles) = data.parse::<Smiles>() {
         let rendered = smiles.to_string();
-        if let Ok(smiles_reparsed) = rendered.parse::<Smiles>() {
-        assert_eq!(smiles, smiles_reparsed);
+
+        if let Ok(reparsed) = rendered.parse::<Smiles>() {
+            let rerendered = reparsed.to_string();
+            assert_eq!(
+                &rendered, &rerendered,
+                "semantic mismatch\ninput: {data:?}\nrendered: {rendered:?}"
+            );
         }
     }
 });
